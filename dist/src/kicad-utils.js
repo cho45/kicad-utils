75a3c985af7e824ace1da20cdd9da08d8a2e9c98
cho45 <cho45@lowreal.net>
2017-08-02 01:22:59 +0900

    update

---
+++ dist/src/kicad-utils.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Common = require("kicad_common");
+const Plotter = require("kicad_plotter");
+const Lib = require("kicad_lib");
+const Sch = require("kicad_sch");
+module.exports = {
+    Common: Common,
+    Plotter: Plotter,
+    Lib: Lib,
+    Sch: Sch,
+};
+//# sourceMappingURL=kicad-utils.js.map
---
+++ src/kicad-utils.ts
@@ -0,0 +1,12 @@
+
+import * as Common from "kicad_common";
+import * as Plotter from "kicad_plotter";
+import * as Lib from "kicad_lib";
+import * as Sch from "kicad_sch";
+
+module.exports = {
+	Common: Common,
+	Plotter: Plotter,
+	Lib: Lib,
+	Sch: Sch,
+};
