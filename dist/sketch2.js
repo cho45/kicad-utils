7b4b699bdbec1d51e6614a14985c393364e02a29
cho45 <cho45@lowreal.net>
2017-08-04 13:18:49 +0900

    ctx

--- dist/sketch2.js
+++ dist/sketch2.js
@@ -8,7 +8,8 @@ const OVERBAR_POSITION_FACTOR = 1.22;
 const BOLD_FACTOR = 1.3;
 const STROKE_FONT_SCALE = 1.0 / 21.0;
 const ITALIC_TILT = 1.0 / 8;
-// common/gal/stroke_font.cpp 
+// common/drawtxt.cpp
+// common/gal/stroke_font.cpp
 class Glyph {
     constructor() {
         this.lines = [];
@@ -18,40 +19,68 @@ class Glyph {
         let rect = new kicad_common_1.Rect(0, 0, this.endX - this.startX, 0);
         for (let line of this.lines) {
             for (let point of line) {
-                console.log(point);
                 rect = rect.merge(new kicad_common_1.Rect(0, 0, this.endX - this.startX, point.y).normalize());
             }
         }
         this.boundingBox = rect;
     }
 }
-const glyphs = [];
-for (let def of kicad_strokefont_1.STROKE_FONT) {
-    const glyph = new Glyph();
-    let points = [];
-    const SERIALIZE_OFFSET = 'R'.charCodeAt(0);
-    const FONT_OFFSET = -10;
-    const glyphStartX = (def.charCodeAt(0) - SERIALIZE_OFFSET) * STROKE_FONT_SCALE;
-    const glyphEndX = (def.charCodeAt(1) - SERIALIZE_OFFSET) * STROKE_FONT_SCALE;
-    for (let i = 2; i < def.length; i += 2) {
-        if (def[i] === ' ' && def[i + 1] === 'R') {
-            // raise pen
-            if (points.length) {
-                glyph.lines.push(points);
-                points.length = 0;
+class GAL {
+    constructor() {
+        this.matrix = {
+            a: 1, b: 0,
+            c: 0, d: 1,
+            tx: 0, ty: 0
+        };
+    }
+    save() {
+    }
+    restore() {
+    }
+    translate() {
+    }
+    scale() {
+    }
+    rotate() {
+    }
+}
+class StrokeFont {
+    constructor() {
+        this.glyphs = [];
+        for (let def of kicad_strokefont_1.STROKE_FONT) {
+            const glyph = new Glyph();
+            let points = [];
+            const SERIALIZE_OFFSET = 'R'.charCodeAt(0);
+            const FONT_OFFSET = -10;
+            const glyphStartX = (def.charCodeAt(0) - SERIALIZE_OFFSET) * STROKE_FONT_SCALE;
+            const glyphEndX = (def.charCodeAt(1) - SERIALIZE_OFFSET) * STROKE_FONT_SCALE;
+            for (let i = 2; i < def.length; i += 2) {
+                if (def[i] === ' ' && def[i + 1] === 'R') {
+                    // raise pen
+                    if (points.length) {
+                        glyph.lines.push(points);
+                        points.length = 0;
+                    }
+                }
+                else {
+                    const x = (def.charCodeAt(i) - SERIALIZE_OFFSET) * STROKE_FONT_SCALE - glyphStartX;
+                    const y = (def.charCodeAt(i + 1) - SERIALIZE_OFFSET + FONT_OFFSET) * STROKE_FONT_SCALE;
+                    points.push(new kicad_common_1.Point(x, y));
+                }
             }
-        }
-        else {
-            const x = (def.charCodeAt(i) - SERIALIZE_OFFSET) * STROKE_FONT_SCALE - glyphStartX;
-            const y = (def.charCodeAt(i + 1) - SERIALIZE_OFFSET + FONT_OFFSET) * STROKE_FONT_SCALE;
-            points.push(new kicad_common_1.Point(x, y));
+            glyph.startX = glyphStartX;
+            glyph.endX = glyphEndX;
+            glyph.computeBoundingBox();
+            this.glyphs.push(glyph);
         }
     }
-    glyph.startX = glyphStartX;
-    glyph.endX = glyphEndX;
-    glyphs.push(glyph);
+    getInterline(size, lineWidth) {
+        return (size * INTERLINE_PITCH_RATIO) + lineWidth;
+    }
+    drawSingleLineText(plotter, text, pos, size, lineWidth, angle, hjustify, vjustify) {
+    }
 }
-const glyph = glyphs['A'.charCodeAt(0) - ' '.charCodeAt(0)];
-glyph.computeBoundingBox();
+const font = new StrokeFont();
+const glyph = font.glyphs['A'.charCodeAt(0) - ' '.charCodeAt(0)];
 console.log(glyph);
 //# sourceMappingURL=sketch2.js.map
--- sketch2.ts
+++ sketch2.ts
@@ -1,7 +1,13 @@
 //#! NODE_PATH=dist/src node dist/sketch2.js 
 
-import { Point, Rect } from "./src/kicad_common";
+import {
+	Point,
+	Rect,
+	TextHjustify,
+	TextVjustify,
+} from "./src/kicad_common";
 import { STROKE_FONT } from "./src/kicad_strokefont";
+import { Plotter } from "./src/kicad_plotter";
 
 const INTERLINE_PITCH_RATIO = 1.5;
 const OVERBAR_POSITION_FACTOR = 1.22;
@@ -9,7 +15,8 @@ const BOLD_FACTOR = 1.3;
 const STROKE_FONT_SCALE = 1.0 / 21.0;
 const ITALIC_TILT = 1.0 / 8;
 
-// common/gal/stroke_font.cpp 
+// common/drawtxt.cpp
+// common/gal/stroke_font.cpp
 class Glyph {
 	lines: Array< Array<Point> >;
 	startX: number;
@@ -25,7 +32,6 @@ class Glyph {
 		let rect = new Rect(0, 0, this.endX - this.startX, 0);
 		for (let line of this.lines) {
 			for (let point of line) {
-				console.log(point);
 				rect = rect.merge(new Rect(0, 0, this.endX - this.startX, point.y).normalize());
 			}
 		}
@@ -33,40 +39,82 @@ class Glyph {
 	}
 }
 
+class GAL {
+	matrix = {
+		a: 1, b: 0,
+		c: 0, d: 1,
+		tx: 0, ty: 0
+	};
 
-const glyphs : Array< Glyph > = [];
-for (let def of STROKE_FONT) {
-	const glyph = new Glyph();
-	let points: Array<Point> = [];
+	history: Array<any>;
 
-	const SERIALIZE_OFFSET = 'R'.charCodeAt(0);
-	const FONT_OFFSET = -10;
+	constructor() {
+	}
 
-	const glyphStartX = (def.charCodeAt(0) - SERIALIZE_OFFSET) * STROKE_FONT_SCALE;
-	const glyphEndX = (def.charCodeAt(1) - SERIALIZE_OFFSET) * STROKE_FONT_SCALE;
+	save() {
+	}
 
-	for (let i = 2; i < def.length; i += 2) {
-		if (def[i] === ' ' && def[i+1] === 'R') {
-			// raise pen
-			if (points.length) {
-				glyph.lines.push(points);
-				points.length = 0;
+	restore() {
+	}
+
+	translate() {
+	}
+
+	scale() {
+	}
+
+	rotate() {
+	}
+}
+
+class StrokeFont {
+	glyphs : Array< Glyph > = [];
+
+	constructor() {
+		for (let def of STROKE_FONT) {
+			const glyph = new Glyph();
+			let points: Array<Point> = [];
+
+			const SERIALIZE_OFFSET = 'R'.charCodeAt(0);
+			const FONT_OFFSET = -10;
+
+			const glyphStartX = (def.charCodeAt(0) - SERIALIZE_OFFSET) * STROKE_FONT_SCALE;
+			const glyphEndX = (def.charCodeAt(1) - SERIALIZE_OFFSET) * STROKE_FONT_SCALE;
+
+			for (let i = 2; i < def.length; i += 2) {
+				if (def[i] === ' ' && def[i+1] === 'R') {
+					// raise pen
+					if (points.length) {
+						glyph.lines.push(points);
+						points.length = 0;
+					}
+				} else {
+					const x = (def.charCodeAt(i) - SERIALIZE_OFFSET) * STROKE_FONT_SCALE - glyphStartX;
+					const y = (def.charCodeAt(i + 1) - SERIALIZE_OFFSET + FONT_OFFSET) * STROKE_FONT_SCALE;
+
+					points.push(new Point(x, y));
+				}
 			}
-		} else {
-			const x = (def.charCodeAt(i) - SERIALIZE_OFFSET) * STROKE_FONT_SCALE - glyphStartX;
-			const y = (def.charCodeAt(i + 1) - SERIALIZE_OFFSET + FONT_OFFSET) * STROKE_FONT_SCALE;
 
-			points.push(new Point(x, y));
+			glyph.startX = glyphStartX;
+			glyph.endX = glyphEndX;
+			glyph.computeBoundingBox();
+
+			this.glyphs.push(glyph);
 		}
 	}
 
-	glyph.startX = glyphStartX;
-	glyph.endX = glyphEndX;
+	getInterline(size: number, lineWidth:number): number {
+		return (size * INTERLINE_PITCH_RATIO ) + lineWidth;
+	}
 
-	glyphs.push(glyph);
+	drawSingleLineText(plotter: Plotter, text: string, pos: Point, size: number, lineWidth: number, angle: number, hjustify: TextHjustify, vjustify: TextVjustify) {
+	}
 }
 
-const glyph = glyphs['A'.charCodeAt(0) - ' '.charCodeAt(0)];
-glyph.computeBoundingBox();
+
+const font = new StrokeFont();
+
+const glyph = font.glyphs['A'.charCodeAt(0) - ' '.charCodeAt(0)];
 console.log(glyph);
 
